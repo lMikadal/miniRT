2c7f839d7ba155b5e1f36049408862292fcdad8e
LMikadaL <panupong.mikada@gmail.com>
2023-07-31 21:46:40 +0700

    update sphere

--- src/render/render.c
+++ src/render/render.c
@@ -2,23 +2,18 @@
 
 t_v3d ray_color(t_ray r)
 {
-	// t_v3d unit_dir;
-	double t;
-
-	t = sphere(r);
-	if (t == 1)
-		return v3d_create(0, 255, 0);
-	return (v3d_create(0, 0, 0));
-	// unit_dir = v3d_unit_vec(r.dir);
-	// t = 0.5 * (unit_dir.y + 1.0);
-	// return (v3d_opr_plus(v3d_mult_double(v3d_create(1.0, 1.0, 1.0), 1.0 - t), v3d_mult_double(v3d_create(0.5, 0.7, 1.0), t)));
+	double t = sphere(r);
+	if (t > 0.0)
+		return (v3d_create(1.0, 0.0, 0.0));
+	t_v3d unit_dir = v3d_unit_vec(r.dir);
+	t = 0.5 * (unit_dir.y + 1.0);
+	return (v3d_opr_plus(v3d_mult_double(v3d_create(1.0, 1.0, 1.0), 1.0 - t), v3d_mult_double(v3d_create(0.5, 0.7, 1.0), t)));
 }
 
 void render(t_mlx *mlx, t_info *info)
 {
 
 	(void)info;
-	(void)mlx;
 
 	// Image size
 	double ratio = 16.0 / 9.0;
@@ -26,7 +21,7 @@ void render(t_mlx *mlx, t_info *info)
 	int height = width / ratio;
 
 	// Camera
-	t_ca camera = create_camera(v3d_create(0.0, 0.0, 0.0), v3d_create(0.0, 0, -1.0), v3d_create(0.0, 1.0, 0.0), 90, ratio);
+	t_ca camera = create_camera(v3d_create(0.0, 0.0, 0.0), v3d_create(0.0, 0, 100.0), v3d_create(0.0, 1.0, 0.0), 90, ratio);
 
 	for (int j = height - 1; j >= 0; --j)
 	{
@@ -37,15 +32,17 @@ void render(t_mlx *mlx, t_info *info)
 
 			t_ray r = get_ray(u, v, camera);
 			t_v3d pix_color = ray_color(r);
+
+			// wirte_color [pix_color];
 			t_rgb rgb;
 			rgb.r = 255.99 * pix_color.x;
 			rgb.g = 255.99 * pix_color.y;
 			rgb.b = 255.99 * pix_color.z;
 
-			if (height - j != height)
-				ft_mlx_pixel_put(mlx, i, height - j, ft_color(rgb));
-			else
-				ft_mlx_pixel_put(mlx, i, 0, ft_color(rgb));
+			// if (height - j != height)
+			ft_mlx_pixel_put(mlx, i, height - j, ft_color(rgb));
+			// else
+			// 	ft_mlx_pixel_put(mlx, i, 0, ft_color(rgb));
 		}
 	}
 }
--- src/shape/sphere.c
+++ src/shape/sphere.c
@@ -2,17 +2,18 @@
 
 int sphere(t_ray r)
 {
-	t_v3d center = v3d_create(50.0, 100.0, -150.0);
-	double radius = 50 / 2;
+	t_v3d center = v3d_create(0.0, 0.0, 10.0);
+	double radius = 10 / 2;
 
 	t_v3d oc = v3d_opr_minus(r.orig, center);
 	double a = v3d_dot(r.dir, r.dir);
 	double b = 2.0 * v3d_dot(oc, r.dir);
 	double c = v3d_dot(oc, oc) - (radius * radius);
 
-	double discrim = (b * b) - (4 * a * c);
+	double discriminant = (b * b) - (4 * a * c);
 
-	if (discrim < 0)
+	if (discriminant < 0)
 		return (0);
-	return (1);
+	else
+		return (((b * -1) - sqrt(discriminant)) / (2.0 * a));
 }
