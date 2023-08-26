b5377cbe6b2c33853327e5e054b460c1ed3d398f
Panupong Mikada <pmikada@z3t3c3.42bangkok.com>
2023-08-27 03:47:05 +0700

    update norm utils

--- src/utils/ft_split.c
+++ src/utils/ft_split.c
@@ -12,16 +12,16 @@
 
 #include "minirt.h"
 
-static int ft_count_word_whitespace(const char *s)
+static int	ft_count_word_whitespace(const char *s)
 {
-	int i;
-	int word;
+	int	i;
+	int	word;
 
 	i = 0;
 	word = 0;
 	while (s[i])
 	{
-		if (ft_whitespace(s[i]) == 0 &&
+		if (ft_whitespace(s[i]) == 0 && \
 			(i == 0 || ft_whitespace(s[i - 1]) == 1))
 			word++;
 		i++;
@@ -29,10 +29,10 @@ static int ft_count_word_whitespace(const char *s)
 	return (word);
 }
 
-int ft_count_word(const char *s, char c)
+int	ft_count_word(const char *s, char c)
 {
-	int i;
-	int word;
+	int	i;
+	int	word;
 
 	i = 0;
 	word = 0;
@@ -45,10 +45,10 @@ int ft_count_word(const char *s, char c)
 	return (word);
 }
 
-static char *ft_loc_string(const char *s, int start, int end)
+static char	*ft_loc_string(const char *s, int start, int end)
 {
-	char *m;
-	int i;
+	char	*m;
+	int		i;
 
 	i = 0;
 	m = (char *)malloc((end - start + 1) * sizeof(char));
@@ -60,12 +60,12 @@ static char *ft_loc_string(const char *s, int start, int end)
 	return (m);
 }
 
-char **ft_split_whitespace(const char *s)
+char	**ft_split_whitespace(const char *s)
 {
-	char **m;
-	int i;
-	int start;
-	int word;
+	char	**m;
+	int		i;
+	int		start;
+	int		word;
 
 	if (s == NULL)
 		return (0);
@@ -76,9 +76,11 @@ char **ft_split_whitespace(const char *s)
 	word = 0;
 	while (i <= ft_strlen(s))
 	{
-		if (ft_whitespace(s[i]) == 0 && (i == 0 || ft_whitespace(s[i - 1]) == 1))
+		if (ft_whitespace(s[i]) == 0 && \
+			(i == 0 || ft_whitespace(s[i - 1]) == 1))
 			start = i;
-		if (i != 0 && ft_whitespace(s[i - 1]) == 0 && (ft_whitespace(s[i]) == 1 || s[i] == '\0'))
+		if (i != 0 && ft_whitespace(s[i - 1]) == 0 && \
+			(ft_whitespace(s[i]) == 1 || s[i] == '\0'))
 			m[word++] = ft_loc_string(s, start, i);
 		i++;
 	}
@@ -86,12 +88,12 @@ char **ft_split_whitespace(const char *s)
 	return (m);
 }
 
-char **ft_split(const char *s, char c)
+char	**ft_split(const char *s, char c)
 {
-	char **m;
-	int i;
-	int start;
-	int word;
+	char	**m;
+	int		i;
+	int		start;
+	int		word;
 
 	if (s == NULL)
 		return (0);
