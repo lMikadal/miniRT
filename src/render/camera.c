d4586f9a25f022697fb9e2ce3e4b03deb6ebf12c
LMikadaL <panupong.mikada@gmail.com>
2023-08-26 11:17:51 +0700

    update norm 25 line

--- src/render/camera.c
+++ src/render/camera.c
@@ -2,35 +2,32 @@
 
 t_ray get_ray(double u, double v, t_ca camera)
 {
+	t_v3d group_d_1;
+	t_v3d group_d_2;
+	t_v3d direct;
 
-	t_v3d group_d_1 = v3d_opr_plus(camera.l_l_c, v3d_mult_double(camera.horizontal, u));
-	t_v3d group_d_2 = v3d_opr_minus(v3d_mult_double(camera.vertical, v), camera.origin);
-	t_v3d direct = v3d_opr_plus(group_d_1, group_d_2);
-
+	group_d_1 = v3d_opr_plus(camera.l_l_c, v3d_mult_double(camera.horizontal, u));
+	group_d_2 = v3d_opr_minus(v3d_mult_double(camera.vertical, v), camera.origin);
+	direct = v3d_opr_plus(group_d_1, group_d_2);
 	return (ray_create(camera.origin, direct));
 }
 
 t_ca create_camera(t_v3d lookfrom, t_v3d lookat, t_v3d vup, double fov, double aspect_ratio)
 {
 	t_ca camera;
+	double view[2];
+	t_v3d w;
+	t_v3d u;
+	t_v3d v;
 
-	double theta = fov * PI / 180.0;
-	// double h = tan(theta / 2);
-	// double viewport_height = 2.0 * h;
-	// double viewport_width = aspect_ratio * viewport_height;
-
-	double w_l = tan(theta / 2);
-	double viewport_width = 2.0 * w_l;
-	double viewport_height = viewport_width / aspect_ratio;
-
-	t_v3d w = v3d_unit_vec(v3d_opr_minus(lookfrom, lookat));
-	t_v3d u = v3d_unit_vec(v3d_cross(vup, w));
-	t_v3d v = v3d_cross(w, u);
-
+	view[0] = 2.0 * tan((fov * PI / 180.0) / 2);
+	view[1] = view[0] / aspect_ratio;
+	w = v3d_unit_vec(v3d_opr_minus(lookfrom, lookat));
+	u = v3d_unit_vec(v3d_cross(vup, w));
+	v = v3d_cross(w, u);
 	camera.origin = v3d_create(lookfrom.x, lookfrom.y, lookfrom.z);
-	camera.horizontal = v3d_mult_double(u, viewport_width);
-	camera.vertical = v3d_mult_double(v, viewport_height);
+	camera.horizontal = v3d_mult_double(u, view[0]);
+	camera.vertical = v3d_mult_double(v, view[1]);
 	camera.l_l_c = v3d_opr_minus(v3d_opr_minus(v3d_opr_minus(camera.origin, v3d_div_double(camera.horizontal, 2)), v3d_div_double(camera.vertical, 2)), w);
-
 	return (camera);
 }
